dd316e204a41485a7c22b25ee536d20a0c99d285
edsaibert <eduarda6saibert@gmail.com>
2024-03-20 10:19:10 -0300

    main functions

---
+++ aula5/main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct person {
+    char id[11];
+    int hacking;
+    int criminal;
+} Person_t;
+
+typedef struct population {
+    Person_t* person;
+    struct population* next;
+} Population_t;
+
+void iniPopulation(Population_t* p){
+    p = NULL;
+}
+
+void iniPerson(Person_t* p){
+    p = NULL;
+}
+
+Population_t* readFile( char* file_name ){
+     Population_t* head;
+     Person_t* person;
+     FILE* file;
+
+     iniPopulation(head);
+     iniPerson(person);
+
+     file = fopen(file_name, "r+");
+     if (!file)
+         return NULL;
+
+     while (!feof(file))
+     {
+        person = (Person_t *)malloc(sizeof(Person_t));
+        if (!person)
+            return NULL;
+
+        fread(person, sizeof(Person_t), 1, file);
+
+        if (head == NULL){
+            head = (Population_t *)malloc(sizeof(Population_t));
+            head->person = person;
+            head->next = NULL;
+        } 
+        else {
+            Population_t* aux;
+            aux = (Population_t *)malloc(sizeof(Population_t));
+            aux->person = person;
+            aux->next = head;
+            head = aux;
+        }
+     }
+
+    fclose(file);
+    return head;
+}
+
+void printInformation( Population_t* head ){
+    Population_t* temp = head;
+    Person_t* person;
+
+    int populationCount = 0;
+    int criminals = 0;
+    float habilityMean;
+
+    while (temp) {
+       person = temp->person;
+
+       printf(" ----------------------------------------- ");
+       prtinf("") 
+    }
+}
+
+int freePopulation(Population_t* head){
+    Population_t* aux = head;
+
+    while (head) {
+        aux = head->next;
+        free(head->person);
+        free(head);
+        head = aux;
+    }
+}
+
+
+int main() {
+    Population_t* head;
+
+    head = readFile("citizens.bin");
+    freePopulation(head);
+
+    return 0;
+}
